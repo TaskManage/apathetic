d1966c7f99d6023fb2cc0f7577522ef07dcef273
Jason Allen <Jason Allen>
2016-06-02 14:51:03 -0600

    quill implemented. Notes page in progress.

--- public/www/app/components/notebook/classNotes/notebookCtrl.js
+++ public/www/app/components/notebook/classNotes/notebookCtrl.js
@@ -1,3 +1,27 @@
-angular.module('studentSuccess').controller('notebookCtrl', function($scope) {
+angular.module('studentSuccess').controller('notebookCtrl', function($scope, notebookService) {
 
-})
+$scope.message = {
+  name: "",
+  message: ""
+}
+
+  $scope.addNote = function(){
+    console.log("hit from fired add note")
+      notebookService.addNote($scope.message).then(function(response){
+        console.log("hit from return", response);
+      // $scope.saveArr.push(response);
+      // $scope.storyId = response._id;
+      // $scope.storyRes = response;
+  })
+}
+});
+
+//   $scope.readUserNote = function(){
+//     storyService.readUserStory($scope.user).then(function(response){
+//       console.log("hit from userList readUserStory");
+//       $scope.story = response;
+//     })
+//   };
+//
+//   $scope.readUserStory();
+// })
