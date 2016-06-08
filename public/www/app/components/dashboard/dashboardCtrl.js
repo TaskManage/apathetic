eacdd3044f1d97273e7b07c19aa7e5ee2d4c92f7
haydenguitar411 <haydenejacobson@gmail.com>
2016-06-08 10:31:01 -0600

    added angular Gravatar, fixed some user edit stuff

--- public/www/app/components/dashboard/dashboardCtrl.js
+++ public/www/app/components/dashboard/dashboardCtrl.js
@@ -12,19 +12,4 @@ angular.module('studentSuccess').controller('dashboardCtrl', function($scope, us
 
   $scope.getTasks();
 
-  $scope.classes = [
-    {
-      name: "Math Class",
-      time: '9am - 10am'
-    },
-    {
-      name: "Biology",
-      time: '10am - 11am'
-    },
-    {
-      name: "Music",
-      time: '11am - 12pm'
-    }
-  ];
-
 });
